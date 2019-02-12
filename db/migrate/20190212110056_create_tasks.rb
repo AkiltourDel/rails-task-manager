bc7752e9f313464b4da41684a8591834df13e611
Guillaume Deleuze <deleuzegui@hotmail.com>
2019-02-12 15:40:42 +0100

    exercice 2

--- db/migrate/20190212110056_create_tasks.rb
+++ db/migrate/20190212110056_create_tasks.rb
@@ -2,8 +2,7 @@ class CreateTasks < ActiveRecord::Migration[5.2]
   def change
     create_table :tasks do |t|
       t.string :title
-      t.details :text
-      t.completed :boolean, default: false
+      t.text :details
 
       t.timestamps
     end
